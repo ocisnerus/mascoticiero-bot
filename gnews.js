0067fffcb8aa2eb4bac72048e5a71812213452ab
ocisnerus <ocisnerus@gmail.com>
2025-04-19 20:24:12 -0600

    Update gnews.js

--- gnews.js
+++ gnews.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
+import axios from "axios";
+import dotenv from "dotenv";
 dotenv.config();
 
-export async function obtenerNoticia() {
-  try {
-    const response = await axios.get(`https://gnews.io/api/v4/search?q=animals&lang=es&country=mx&max=1&token=${process.env.GNEWS_API}`);
-    return response.data.articles[0];
-  } catch (error) {
-    throw new Error(`❌ Error al obtener noticia: ${error.message}`);
-  }
+export async function getNews() {
+  const apiKey = process.env.GNEWS_API;
+  const response = await axios.get(
+    `https://gnews.io/api/v4/search?q=mascotas OR animales&lang=es&country=mx&max=1&token=${apiKey}`
+  );
+  return response.data.articles[0];
 }
